5bc756bf12adba7322dddb81927a0346d0af951e
Nick Wang <nickyiliwang@gmail.com>
2020-07-05 23:40:35 -0400

    getAuctions route at GET /auctions, using scan on AuctionsTable Table name in dynamo db

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,21 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
+// before common middleware
+import middy from "@middy/core";
+// no need for JSON.parse, auto json parse
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+// Normalizer, prevents throwing error if no prop, just shows undefined
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  // before httpJsonBodyParser
+  // const { title } = JSON.parse(event.body);
+  // after
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
@@ -13,14 +24,18 @@ async function createAuction(event, context) {
     status: "OPEN",
     createdAt: now.toISOString(),
   };
-
-  // Waiting for the put action to finish, and return a promise, await the promise to finish then return.
-  await dynamodb
-    .put({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction,
-    })
-    .promise();
+  try {
+    // Waiting for the put action to finish, and return a promise, await the promise to finish then return/ for error handling
+    await dynamodb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -28,4 +43,8 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+// using middy here
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
